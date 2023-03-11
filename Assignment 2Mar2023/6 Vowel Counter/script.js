bca1053337c8ac7116b6855a8f35cf6b09ca9919
Anshu Shree <121965174+AnshuShree2004@users.noreply.github.com>
2023-03-11 11:22:26 +0530

    script.js

--- Assignment 2Mar2023/6 Vowel Counter/script.js
+++ Assignment 2Mar2023/6 Vowel Counter/script.js
@@ -1,1 +1,18 @@
+//vowels
+function countVowels(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let count = 0;
+  
+  for(let i = 0; i < str.length; i++) {
+    if(vowels.includes(str[i].toLowerCase())) {
+      count++;
+    }
+  }
+  
+  return count;
+}
+const str = "Hello, World!";
+const vowelCount = countVowels(str);
+console.log(`The string "${str}" contains ${vowelCount} vowels.`);
+// Output: The string "Hello, World!" contains 3 vowels.
 
