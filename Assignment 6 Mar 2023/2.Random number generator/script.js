e93b7856e3685151abc2aafd0c70e5053eec8191
Anshu Shree <121965174+AnshuShree2004@users.noreply.github.com>
2023-03-13 13:53:36 +0530

    script.js

--- Assignment 6 Mar 2023/2.Random number generator/script.js
+++ Assignment 6 Mar 2023/2.Random number generator/script.js
@@ -1,1 +1,18 @@
 
+const delay = 3; 
+
+console.log(`Generating random number in ${delay} seconds...`);
+
+// count down the time until the random number is generated
+let timeRemaining = delay;
+const countdownInterval = setInterval(() => {
+  timeRemaining--;
+  console.log(`${timeRemaining} seconds remaining...`);
+  if (timeRemaining === 0) {
+    clearInterval(countdownInterval);
+
+    // generate the random number
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(`Random number generated: ${randomNumber}`);
+  }
+}, 1000);
