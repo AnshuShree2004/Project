3bd216bd9cc0c0b78691ef295d33ecb6b370541a
Anshu Shree <121965174+AnshuShree2004@users.noreply.github.com>
2023-03-13 13:49:51 +0530

    script.js

--- Assignment 6 Mar 2023/1.Reverse String/script.js
+++ Assignment 6 Mar 2023/1.Reverse String/script.js
@@ -1,1 +1,7 @@
 
+function reverseString(str) {
+  let reversedString=str.split('').reverse().join('')
+console.log(reversedString);
+}
+let input="Hey girl,you are beautiful!"
+setTimeout(reverseString(input),2000)
