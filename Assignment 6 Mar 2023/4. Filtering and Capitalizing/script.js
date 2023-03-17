2d938948e78fb6deda6b102483661f63bb12e959
Anshu Shree <121965174+AnshuShree2004@users.noreply.github.com>
2023-03-17 08:00:22 +0530

    script.js

--- Assignment 6 Mar 2023/4. Filtering and Capitalizing/script.js
+++ Assignment 6 Mar 2023/4. Filtering and Capitalizing/script.js
@@ -1,20 +1,22 @@
 const books = [
-  { title: "The Catcher in the Rye", author: "J.D. Salinger", year: 1951 },
-  { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
-  { title: "1984", author: "George Orwell", year: 1949 },
-  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
-  { title: "The Hunger Games", author: "Suzanne Collins", year: 2008 },
-  { title: "Pride and Prejudice", author: "Jane Austen", year: 1813 },
-  { title: "The Hobbit", author: "J.R.R. Tolkien", year: 1937 }
+  {title: "The Hunger Games", author: "Suzanne Collins", year: 2018},
+  {title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960},
+  {title: "1984", author: "George Orwell", year: 1949},
+  {title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925},
+  {title: "The Catcher in the Rye", author: "J.D. Salinger", year: 1951},
+  {title: "The Lord of the Rings", author: "J.R.R. Tolkien", year: 1954},
+  {title: "Pride and Prejudice", author: "Jane Austen", year: 1813},
+  {title: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", year: 1979}
 ];
 
-const filteredBooks = books.filter(book => book.year >= 2010)
-                          .map(book => {
-                            return {
-                              title: book.title,
-                              author: book.author.toUpperCase(),
-                              year: book.year
-                            }
-                          });
+const filteredBooks = books.filter(book => book.year >= 2010);
+const capitalizedBooks = filteredBooks.map(book => {
+  return {
+    title: book.title,
+    author: book.author.toUpperCase(),
+    year: book.year
+  };
+});
 
-console.log(filteredBooks);
+console.log(capitalizedBooks);
+
