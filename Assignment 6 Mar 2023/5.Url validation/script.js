d14d3f2e287e6a57c3f85b417f329799bc24e8fb
Anshu Shree <121965174+AnshuShree2004@users.noreply.github.com>
2023-03-17 07:18:01 +0530

    script.js

--- Assignment 6 Mar 2023/5.Url validation/script.js
+++ Assignment 6 Mar 2023/5.Url validation/script.js
@@ -1,5 +1,10 @@
-let input="https://www.google.com"
-  let regx=/^(https?:\/\/)?([\da-z\.-]+\.[a-z\.]{2,6}|[\d\.]+)([\/:?=&#]{1}[\da-z\.-]+)*[\/\?]?$/igm
-
-let toMatchInput = input.match(regx)
-console.log(toMatchInput)
+const urlReg=/^https?:\/\/[\w\d\S]+?\.[\w]+$/gim;
+function isValidURL(url){
+    if(urlReg.test(url)){
+        console.log(`The URL ${url} is valid.`)
+    }
+    else{
+        console.log(`The URL ${url} is not valid.`)
+    }
+}
+isValidURL("https://www.example.com")
